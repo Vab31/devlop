4ccbf6904f659f68619f1476ef69184195c29c78
Vaibhav Singh <singhvaibhav654@gmail.com>
2023-09-25 23:29:12 +0530

    update error

--- src/index.js
+++ src/index.js
@@ -6,36 +6,36 @@ import { Analytics } from '@vercel/analytics/react';
 import ReactGA from "react-ga4";
 // import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <div className="index">
-//       <App />
-//       <Analytics />
-      
-//     </div>
-//   </React.StrictMode>
-// );
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') 
-);
-//Initialize GA4
-ReactGA.initialize("G-RXSQ0HH7M8");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+    <div className="index">
       <App />
       <Analytics />
-    {/* </BrowserRouter> */}
-
+      
+    </div>
   </React.StrictMode>
 );
-const SendAnalytics = ()=> {
-  ReactGA.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-  });
-}
 
-reportWebVitals(SendAnalytics);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') 
+// );
+//Initialize GA4
+// ReactGA.initialize("G-RXSQ0HH7M8");
+// root.render(
+//   <React.StrictMode>
+//     {/* <BrowserRouter> */}
+//       <App />
+//       <Analytics />
+//     {/* </BrowserRouter> */}
+
+//   </React.StrictMode>
+// );
+// const SendAnalytics = ()=> {
+//   ReactGA.send({
+//     hitType: "pageview",
+//     page: window.location.pathname,
+//   });
+// }
+
+// reportWebVitals(SendAnalytics);
